5117013c3009333a85e22998e25e47a44addae08
gwathlobal <gwathlobal@yandex.ru>
2019-06-01 17:18:46 +0300

    Angels now have names

---
+++ Assets/Scripts/Abilities/Common/AbilityAngel.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbilityAngel : Ability
+{
+
+    public AbilityAngel()
+    {
+        id = AbilityTypeEnum.abilAngel;
+        stdName = "Angel";
+        passive = true;
+        slot = AbilitySlotEnum.abilNone;
+        category = AbilityPlayerCategoryEnum.abilCommon;
+    }
+
+    public override string Description(Mob mob)
+    {
+        return "This one belongs to the Thirdborn, the divine and spiritual beings.";
+    }
+
+    public override string Name(Mob mob)
+    {
+        return stdName;
+    }
+
+    public override float Spd(Mob mob)
+    {
+        return 0;
+    }
+
+    public override int Cost(Mob mob)
+    {
+        return 0;
+    }
+
+    public override bool DoesMapCheck(Mob mob)
+    {
+        return false;
+    }
+
+    public override bool AbilityCheckAI(Ability ability, Mob actor, Mob nearestEnemy, Mob nearestAlly)
+    {
+        return false;
+    }
+
+    public override bool AbilityCheckApplic(Ability ability, Mob mob)
+    {
+        return true;
+    }
+
+    public override void AbilityInvoke(Mob actor, TargetStruct target)
+    {
+        
+    }
+
+    public override void AbilityInvokeAI(Ability ability, Mob actor, Mob nearestEnemy, Mob nearestAlly)
+    {
+
+    }
+
+    public override bool AbilityMapCheck(Ability ability)
+    {
+        return false;
+    }
+
+    public override bool CheckRequirements(Mob mob, List<AbilityTypeEnum> addedAbils)
+    {
+        return true;
+    }
+}
